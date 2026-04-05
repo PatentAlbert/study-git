fba894d91f30d3956901570d187a4ed8e67b6dee
PatentAlbert <zn2166105337@gmail.com>
2026-04-05 16:32:30 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 print(123)
+<<<<<<< HEAD
 print("Hello, World!")
 print("This is a test file for Git.")
 print("Let's see how Git tracks changes to this file.")
+=======
+print("Hello, World!")
+>>>>>>> parent of f671e72 (Update test.py)
