82706d3295f3bc5037e59c97ca776c0619101c31
PatentAlbert <zn2166105337@gmail.com>
2026-04-05 16:32:51 +0800

    Revert "Update test.py"
    This reverts commit fba894d91f30d3956901570d187a4ed8e67b6dee.

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
 print(123)
-<<<<<<< HEAD
 print("Hello, World!")
 print("This is a test file for Git.")
 print("Let's see how Git tracks changes to this file.")
-=======
-print("Hello, World!")
->>>>>>> parent of f671e72 (Update test.py)
